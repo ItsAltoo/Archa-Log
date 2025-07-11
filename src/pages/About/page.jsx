bac25886758748f9be68c8ec9c35e6e25dab32c8
ItsAltoo <itsmalik57@gmail.com>
2025-07-11 20:34:27 +0800

    refactor: Clean up About page structure and improve image handling

--- src/pages/About/page.jsx
+++ src/pages/About/page.jsx
@@ -24,21 +24,40 @@ const About = () => {
             {...anim(CloseDown)}
           />
         </div>
-
-        <div className="bg-[url(./assets/About/bg_1.webp)] h-full w-full bg-cover top-0 right-0 -z-10 absolute bg-black/80 brightness-[20%] grayscale inset-shadow-sm inset-shadow-background/80" />
+        <div className="h-full w-full  top-0 right-0 -z-10 absolute bg-black/80">
+            <img
+              src="./assets/About/bg_1.webp"
+              className="h-full w-full brightness-[20%] grayscale inset-shadow-sm inset-shadow-background/80"
+            />
+          </div>
 
         <div className="overflow-hidden absolute inset-0">
-        {/* leaf */}
-        <img src=".\assets\About\leaf_ 1.webp" className="w-80 absolute bottom-0 right-0 grayscale z-10 brightness-[10%]"/>
-        <img src=".\assets\About\leaf_ 2.webp" className="w-80 absolute bottom-15 left-0 grayscale z-10 brightness-[10%]"/>
+          
 
-        {/* Dinosaurs */}
-        <img src="\assets\About\dino_ (2).webp" className="w-80 absolute top-34 left-9 grayscale z-10 brightness-[50%] scale-x-[-1]"/>
-        <img src="\assets\About\dino_ (1).webp" className="w-96 absolute top-[45%] -right-7 grayscale z-10 brightness-[50%] "/>
-        <img src="\assets\About\dino_ (3).webp" className="w-80 absolute bottom-1/4 -left-8 grayscale z-10 brightness-[50%]"/>
+          {/* leaf */}
+          <img
+            src=".\assets\About\leaf_ 1.webp"
+            className="w-80 absolute bottom-0 right-0 grayscale z-10 brightness-[10%]"
+          />
+          <img
+            src=".\assets\About\leaf_ 2.webp"
+            className="w-80 absolute bottom-15 left-0 grayscale z-10 brightness-[10%]"
+          />
 
+          {/* Dinosaurs */}
+          <img
+            src="\assets\About\dino_ (2).webp"
+            className="w-80 absolute top-34 left-9 grayscale z-10 brightness-[50%] scale-x-[-1]"
+          />
+          <img
+            src="\assets\About\dino_ (1).webp"
+            className="w-96 absolute top-[45%] -right-7 grayscale z-10 brightness-[50%] "
+          />
+          <img
+            src="\assets\About\dino_ (3).webp"
+            className="w-80 absolute bottom-1/4 -left-8 grayscale z-10 brightness-[50%]"
+          />
         </div>
-        
 
         <Animate delay={1} animate="ZoomIn">
           <h1 className="font-title text-9xl text-center py-20 text-primary-100 text-shadow-sm text-shadow-primary-300 ">
@@ -55,4 +74,3 @@ const About = () => {
 };
 
 export default About;
-  
